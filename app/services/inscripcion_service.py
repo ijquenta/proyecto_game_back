d714e42cf94e82f9fcd9b3b13a573f2c57fa1969
ijquenta <ivanjosuequentavargas@gmail.com>
2024-03-31 16:57:13 -0400

    feat: set dates inscripcion

--- app/services/inscripcion_service.py
+++ app/services/inscripcion_service.py
@@ -1,28 +1,50 @@
 from core.database import select, as_string, execute, execute_function
 from psycopg2 import sql
 from flask import jsonify, make_response
+from datetime import datetime
+
+def darFormatoFechaConHora(fecha_str):
+    if fecha_str is None:
+       return None
+    fecha_datetime = datetime.strptime(fecha_str, "%Y-%m-%dT%H:%M:%S.%fZ")
+    fecha_formateada = fecha_datetime.strftime("%d/%m/%Y %H:%M:%S")
+    return fecha_formateada
+
+def darFormatoFechaSinHora(fecha_str):
+    if not fecha_str:
+        return None
+    fecha_datetime = datetime.strptime(fecha_str, "%Y-%m-%dT%H:%M:%S.%fZ")
+    fecha_formateada = fecha_datetime.strftime("%d/%m/%Y")
+    return fecha_formateada
 
 def listarInscripcion():
-    return select(f'''
-   	SELECT 
-    i.insid, 
-    i.matrid, m.matrgestion, m.matrestado, m.matrestadodescripcion, 
-    i.peridestudiante, p.pernomcompleto as pernombrecompletoestudiante, 
-    i.pagid, p2.pagdescripcion,  p2.pagestado, p2.pagestadodescripcion, p2.pagmonto, 
-    i.curmatid, cm.curid, cm.curmatdescripcion, c.curnombre, cm.matid, m2.matnombre,
-    cm.periddocente, p3.pernomcompleto as pernombrecompletodocente,
-    i.insusureg, i.insfecreg, i.insusumod, i.insfecmod, 
-    i.insestado, i.insestadodescripcion
-    FROM academico.inscripcion i
-    left join academico.matricula m on i.matrid = m.matrid
-    left join academico.persona p on i.peridestudiante = p.perid 
-    left join academico.pago p2 on i.pagid = p2.pagid
-    left join academico.curso_materia cm on i.curmatid = cm.curmatid   
-    left join academico.curso c on cm.curid = c.curid
-    left join academico.materia m2 on cm.matid = m2.matid
-    left join academico.persona p3 on cm.periddocente = p3.perid
-    order by i.insid desc;
+    lista_inscripciones = select(f'''
+        SELECT 
+        i.insid, 
+        i.matrid, m.matrgestion, m.matrestado, m.matrestadodescripcion, 
+        i.peridestudiante, p.pernomcompleto as pernombrecompletoestudiante, p.perfoto, 
+        i.pagid, p2.pagdescripcion,  p2.pagtipo, p2.pagmonto, p2.pagfecha, p2.pagarchivo,
+        i.curmatid, cm.curid, cm.curmatdescripcion, c.curnombre, cm.matid, m2.matnombre,
+        cm.periddocente, p3.pernomcompleto as pernombrecompletodocente,
+        i.insusureg, i.insfecreg, i.insusumod, i.insfecmod, 
+        i.insestado, i.insestadodescripcion
+        FROM academico.inscripcion i
+        left join academico.matricula m on i.matrid = m.matrid
+        left join academico.persona p on i.peridestudiante = p.perid 
+        left join academico.pago p2 on i.pagid = p2.pagid
+        left join academico.curso_materia cm on i.curmatid = cm.curmatid   
+        left join academico.curso c on cm.curid = c.curid
+        left join academico.materia m2 on cm.matid = m2.matid
+        left join academico.persona p3 on cm.periddocente = p3.perid
+        order by i.insid desc;
     ''')
+    print("lista_inscripciones: ", lista_inscripciones)
+    for curso in lista_inscripciones:
+        curso["insfecreg"] = darFormatoFechaConHora(curso["insfecreg"])
+        curso["insfecmod"] = darFormatoFechaConHora(curso["insfecmod"])
+    print(lista_inscripciones)
+    return lista_inscripciones
+
        
     
 def listarComboCursoMateria():
