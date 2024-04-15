ffbafd8951236aec3bdf89f4ea2217bdc7112515
ijquenta <ivanjosuequentavargas@gmail.com>
2024-04-15 16:38:47 -0400

    update url posgresql

--- app/core/configuration.py
+++ app/core/configuration.py
@@ -3,7 +3,8 @@ import os
 APP_VERSION = os.getenv("APP_VERSION", "0.1.0")
 # SQLALCHEMY_DATABASE_URL =os.getenv('SQLALCHEMY_DATABASE_URL','postgresql://postgres:gimena@127.0.0.1:5432/aguinaldo_db')
 # SQLALCHEMY_DATABASE_URL = os.getenv('SQLALCHEMY_DATABASE_URL','postgresql://usu_reintegro:u5u_r3in73gR0@200.7.161.176:5432/bd_reintegro')
-SQLALCHEMY_DATABASE_URL = os.getenv('SQLALCHEMY_DATABASE_URL','postgresql://admin:123456@host.docker.internal:5432/db_academico')
+# SQLALCHEMY_DATABASE_URL = os.getenv('SQLALCHEMY_DATABASE_URL','postgresql://admin:123456@host.docker.internal:5432/db_academico')
+SQLALCHEMY_DATABASE_URL = os.getenv('SQLALCHEMY_DATABASE_URL','postgresql://postgres:yFnqNwRDqSaHmpvEVQnlsmWNpkHzYNrh@viaduct.proxy.rlwy.net:48498/railway')
 # SQLALCHEMY_DATABASE_URL = "postgresql://admin:123456@host.docker.internal:5432/db_academico"
 
 APP_SECRET_KEY = os.getenv("APP_SECRET_KEY", "secret")
