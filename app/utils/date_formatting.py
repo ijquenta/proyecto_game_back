480d0ad95486b15177f5e3f4873449b54f50f7f0
ijquenta <ivanjosuequentavargas@gmail.com>
2024-04-17 12:48:08 -0400

    add date_formatting

--- app/utils/date_formatting.py
+++ app/utils/date_formatting.py
@@ -13,3 +13,11 @@ def darFormatoFechaSinHora(fecha_str):
     fecha_datetime = datetime.strptime(fecha_str, "%Y-%m-%dT%H:%M:%S.%fZ")
     fecha_formateada = fecha_datetime.strftime("%d/%m/%Y")
     return fecha_formateada
+
+
+def darFormatoFechaNacimiento(fecha_str):
+    if not fecha_str:
+        return None
+    fecha_datetime = datetime.strptime(fecha_str, "%Y-%m-%dT%H:%M:%S.%fZ")
+    fecha_formateada = fecha_datetime.strftime("%d/%m/%Y")
+    return fecha_formateada 
