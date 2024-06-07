c5c06efec0d6120306991b04c2a1e64a6b33b900
ijquenta <ivanjosuequentavargas@gmail.com>
2024-06-07 16:10:31 -0400

    create resource access crud and others

---
+++ app/resources/Acceso_resource.py
@@ -0,0 +1,40 @@
+from flask_restful import Resource, reqparse
+from services.acceso_service import * # Servicio de acceso
+
+class GetAccesos(Resource):
+    def get(self):
+        return list_all_accesses()
+    
+class GetSubMenus(Resource):
+    def get(self):
+        return getSubMenus()
+    
+class GetSubMenuType(Resource):
+    def get(self):
+        return getSubMenuType()
+    
+parseCreateAccess = reqparse.RequestParser()
+parseCreateAccess.add_argument('rolid', type=int, help='Ingrese rolid', required=True)
+parseCreateAccess.add_argument('submenid', type=int, help='Ingrese submenuid', required=True)
+parseCreateAccess.add_argument('accactivo', type=int, help='Ingrese accactivo', required=True)
+parseCreateAccess.add_argument('accusureg', type=str, help='Ingrese accusureg', required=True)
+parseCreateAccess.add_argument('accdescripcion', type=str, help='Ingrese accdescripcion', required=False)
+parseCreateAccess.add_argument('accestado', type=int, help='Ingrese accestado', required=True)
+class CreateAccess(Resource):
+    def post(self):
+        data = parseCreateAccess.parse_args()
+        return createAccess(data)
+    
+parseUpdateAccess = reqparse.RequestParser()
+parseUpdateAccess.add_argument('accactivo', type=int, help='Ingrese accactivo', required=True)
+parseUpdateAccess.add_argument('accusumod', type=str, help='Ingrese accusumod', required=True)  
+class UpdateAccess(Resource):
+    def put(self, accid):
+        data = parseUpdateAccess.parse_args()
+        return updateAccess(data, accid)
+    
+parseDeleteAccess = reqparse.RequestParser()
+class DeleteAccess(Resource):
+    def delete(self, accid):
+        return deleteAccess(accid)
+    
