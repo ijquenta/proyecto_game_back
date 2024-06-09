e50c92c6bcb3aba64be7f24a5c75e3c75433819f
ijquenta <ivanjosuequentavargas@gmail.com>
2024-06-09 02:02:24 -0400

    add routes with resources operations

--- app/routes/operacion_routes.py
+++ app/routes/operacion_routes.py
@@ -4,3 +4,7 @@ from client.routes import Routes as routes
 
 def operacion_routes(api: Api):
     api.add_resource(operacion.GetTipoOperacion, routes.getTipoOperacion)
+    api.add_resource(operacion.GetOperations, routes.getOperations)
+    api.add_resource(operacion.CreateOperation, routes.createOperation)
+    api.add_resource(operacion.UpdateOperation, routes.updateOperation)
+    api.add_resource(operacion.DeleteOperation, routes.deleteOperation)
