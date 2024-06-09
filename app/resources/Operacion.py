928fb34c8a458d7e303f070dd66747b877af2e1c
ijquenta <ivanjosuequentavargas@gmail.com>
2024-06-09 02:01:53 -0400

    add resource operation

--- app/resources/Operacion.py
+++ app/resources/Operacion.py
@@ -2,6 +2,60 @@ from flask_restful import Resource, reqparse
 from services.permiso_service import * # Servicio de permiso
 from services.operacion_service import *
 
+class GetOperations(Resource):
+    def get(self):
+        return getOperations()
+    
+
+parseCreateOperation = reqparse.RequestParser()
+parseCreateOperation.add_argument('openombre', type=str, help='Ingrese openombre', required=True)
+parseCreateOperation.add_argument('opeusureg', type=str, help='Ingrese opeusureg', required=True)
+parseCreateOperation.add_argument('opedescripcion', type=str, help='Ingrese opedescripcion', required=False)
+parseCreateOperation.add_argument('opeestado', type=int, help='Ingrese opeestado', required=True)
+class CreateOperation(Resource):
+    def post(self):
+        data = parseCreateOperation.parse_args()
+        return createOperation(data)
+
+parseUpdateOperation = reqparse.RequestParser()   
+parseUpdateOperation.add_argument('openombre', type=str, help='Ingrese openombre', required=False)  
+parseUpdateOperation.add_argument('opeusumod', type=str, help='Ingrese opeusumod', required=True)
+parseUpdateOperation.add_argument('opedescripcion', type=str, help='Ingrese opedescripcion', required=False)
+parseUpdateOperation.add_argument('opeestado', type=int, help='Ingrese opestado', required=False)
+class UpdateOperation(Resource):
+    def put(self, opeid):
+        data = parseUpdateOperation.parse_args()
+        return updateOperation(data, opeid)
+    
+class DeleteOperation(Resource):
+    def delete(self, opeid):
+        return deleteOperation(opeid)
+
+
+parseCreateAccess = reqparse.RequestParser()
+parseCreateAccess.add_argument('rolid', type=int, help='Ingrese rolid', required=True)
+parseCreateAccess.add_argument('submenid', type=int, help='Ingrese submenuid', required=True)
+parseCreateAccess.add_argument('accactivo', type=int, help='Ingrese accactivo', required=True)
+parseCreateAccess.add_argument('accusureg', type=str, help='Ingrese accusureg', required=True)
+parseCreateAccess.add_argument('accdescripcion', type=str, help='Ingrese accdescripcion', required=False)
+parseCreateAccess.add_argument('accestado', type=int, help='Ingrese accestado', required=True)
+class CreateAccess(Resource):
+    def post(self):
+        data = parseCreateAccess.parse_args()
+        return createAccess(data)
+    
+parseUpdateAccess = reqparse.RequestParser()
+parseUpdateAccess.add_argument('accactivo', type=int, help='Ingrese accactivo', required=True)
+parseUpdateAccess.add_argument('accusumod', type=str, help='Ingrese accusumod', required=True)  
+class UpdateAccess(Resource):
+    def put(self, accid):
+        data = parseUpdateAccess.parse_args()
+        return updateAccess(data, accid)
+    
+parseDeleteAccess = reqparse.RequestParser()
+class DeleteAccess(Resource):
+    def delete(self, accid):
+        return deleteAccess(accid)
 
 class GetTipoOperacion(Resource):
     def get(self):
@@ -9,43 +63,3 @@ class GetTipoOperacion(Resource):
 
 
 
-class ListarPermiso(Resource):
-    def get(self):
-        return listarPermiso()
-
-class ListarPermisoRol(Resource):
-    def get(self):
-        return "listarPermisoRol"
-
-class GetPermisos(Resource):
-    def get(self):
-        return getPermisos()
-    
-class GetRoles(Resource):
-    def get(self):
-        return getRoles()
-    
-class GetOperaciones(Resource):
-    def get(self):
-        return getOperaciones()
-    
-parseUpdatePermiso = reqparse.RequestParser()
-parseUpdatePermiso.add_argument('permid', type=int, help='Ingrese permid', required=True)
-parseUpdatePermiso.add_argument('permactivo', type=int, help='Ingrese permactivo', required=True)
-parseUpdatePermiso.add_argument('permusumod', type=str, help='Ingrese permusureg', required=True)
-class UpdatePermiso(Resource):
-    def post(self):
-        data = parseUpdatePermiso.parse_args()
-        return updatePermiso(data)
-
-parseAddPermiso = reqparse.RequestParser()
-parseAddPermiso.add_argument('rolid', type=int, required=True, help='Ingrese rolid')
-parseAddPermiso.add_argument('opeid', type=int, required=True, help='Ingrese opeid')
-parseAddPermiso.add_argument('permactivo', type=int, required=True, help='Ingrese permactivo')
-parseAddPermiso.add_argument('permusureg', type=str, required=True, help='Ingrese permusureg')
-parseAddPermiso.add_argument('permdescripcion', type=str, required=False, help='Ingrese permdescripcion')
-parseAddPermiso.add_argument('permestado', type=int, default=1, help='Ingrese permestado')
-class AddPermiso(Resource):
-    def post(self):
-        data = parseAddPermiso.parse_args()
-        return addPermiso(data)
