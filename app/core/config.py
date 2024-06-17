f51ef615305dd0cd06061e27ccd0ea391cdc9273
ijquenta <ivanjosuequentavargas@gmail.com>
2024-06-17 16:39:26 -0400

    add config in core

--- app/core/config.py
+++ app/core/config.py
@@ -3,6 +3,6 @@ from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://admin:123456@192.168.0.41/db_academico'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://admin:123456@localhost:5432/db_ibci'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
