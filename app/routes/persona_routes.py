e585731d4181a7ea1020e9df04b80a9f00f1bbd9
ijquenta <ivanjosuequentavargas@gmail.com>
2024-06-17 16:39:02 -0400

    change name persona resource

--- app/routes/persona_routes.py
+++ app/routes/persona_routes.py
@@ -1,17 +1,16 @@
 from flask_restful import Api
-import resources.Persona as Person
-import resources.Usuario as Usuario
+import resources.Persona_resource as Persona
 import resources.Estudiante as Estudiante   
 from client.routes import Routes as routes
 
 def persona_routes(api: Api):
-    api.add_resource(Person.ListarPersona, routes.listarPersona)
-    api.add_resource(Person.GestionarPersona, routes.gestionarPersona)
-    api.add_resource(Person.EliminarPersona, routes.eliminarPersona)
-    api.add_resource(Person.TipoDocumento, routes.tipoDocumento)
-    api.add_resource(Person.TipoEstadoCivil, routes.tipoEstadoCivil)
-    api.add_resource(Person.TipoGenero, routes.tipoGenero)
-    api.add_resource(Person.TipoPais, routes.tipoPais)
-    api.add_resource(Person.TipoCiudad, routes.tipoCiudad)
-    api.add_resource(Person.RegistrarPersona, routes.registrarPersona)
+    api.add_resource(Persona.ListarPersona, routes.listarPersona)
+    api.add_resource(Persona.GestionarPersona, routes.gestionarPersona)
+    api.add_resource(Persona.EliminarPersona, routes.eliminarPersona)
+    api.add_resource(Persona.TipoDocumento, routes.tipoDocumento)
+    api.add_resource(Persona.TipoEstadoCivil, routes.tipoEstadoCivil)
+    api.add_resource(Persona.TipoGenero, routes.tipoGenero)
+    api.add_resource(Persona.TipoPais, routes.tipoPais)
+    api.add_resource(Persona.TipoCiudad, routes.tipoCiudad)
+    api.add_resource(Persona.RegistrarPersona, routes.registrarPersona)
     api.add_resource(Estudiante.ActualizarDatosPersonales, routes.actualizarDatosPersonales)
