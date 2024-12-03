b9eafea08695e1b80ca002f0448c00e22317d13a
Ivan Josue Quenta Vargas <ivanjosuequentavargas@gmail.com>
2024-12-03 18:40:19 -0400

    limpieza

--- app/routes/reporte_routes.py
+++
@@ -1,8 +0,0 @@
-from flask_restful import Api
-import resources.Reportes as Report
-from client.routes import Routes as routes
-
-def reporte_routes(api: Api):
-    api.add_resource(Report.rptTotalesSigma, routes.rptTotalesSigma)
-    api.add_resource(Report.rptCursoMateriaContabilidad, routes.rptCursoMateriaContabilidad)
-    api.add_resource(Report.RptInformacionAdmision, routes.rptInformacionAdmision)
